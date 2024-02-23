312412509752ff400b95b8fa6d9f9c8601a62169
OKAMOTO Shigehiro <primary@goahi.live>
2024-02-23 20:08:18 +0900

    feat: implement getLastWeekContributions

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 	}
 
 	graphqlClient := graphql.NewClient("https://api.github.com/graphql", &httpClient)
-	contributions, err := getLastWeekContributions(graphqlClient, "oka4shi")
+	contributions, err := getLastWeekContributions(graphqlClient, time.Now(), "oka4shi")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -57,19 +57,18 @@ func main() {
 
 }
 
-func getDateRange(daysBefore int) dateRange {
-	now := time.Now()
-
+func getDateRange(now time.Time, origin int, duration int) dateRange {
 	return dateRange{
-		To:   now,
-		From: now.AddDate(0, 0, -daysBefore),
+		To:   now.AddDate(0, 0, -origin),
+		From: now.AddDate(0, 0, -origin-(duration-1)),
 	}
 }
 
 type weeklyContributions = getUserContributionsUserContributionsCollectionContributionCalendarWeeksContributionCalendarWeek
 
-func getLastWeekContributions(c graphql.Client, user string) (weeklyContributions, error) {
-	r := getDateRange(7)
+func getLastWeekContributions(c graphql.Client, now time.Time, user string,) (weeklyContributions, error) {
+	d := (int(now.Weekday())+1) % 7
+	r := getDateRange(now, d, 7)
 
 	var resp *getUserContributionsResponse
 	resp, err := getUserContributions(context.Background(), c, user, r.To, r.From)
