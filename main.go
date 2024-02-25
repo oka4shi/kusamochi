55222ffc05093a8bde6d97b3b6aa1682c940216f
OKAMOTO Shigehiro <primary@goahi.live>
2024-02-25 21:52:08 +0900

    feat: implement ranking

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/Khan/genqlient/graphql"
@@ -12,6 +16,11 @@ import (
 type dateRange struct {
 	To   time.Time
 	From time.Time
+}
+
+type person struct {
+	Name          string
+	Contributions int
 }
 
 type authedTransport struct {
@@ -31,13 +40,59 @@ func main() {
 		return
 	}
 
-	contributions, err := getLastWeekContributions(client, time.Now(), "oka4shi")
+	f, err := os.Open("users.json")
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatalln(err)
 	}
-	fmt.Printf("%v", contributions)
+	defer f.Close()
 
+	jsonStr, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var users []string
+	err = json.Unmarshal([]byte(jsonStr), &users)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var duration dateRange
+
+	var data []person
+	for i, user := range users {
+		contributions, err := getLastWeekContributions(client, time.Now(), user)
+		if err != nil {
+			log.Println(err)
+		}
+
+		var contributionsSum int
+		for _, v := range contributions {
+			contributionsSum += v.ContributionCount
+		}
+		data = append(data, person{
+			Name:          user,
+			Contributions: contributionsSum,
+		})
+		if i == 0 {
+			duration = dateRange{
+				From: contributions[0].Date,
+				To:   contributions[len(contributions)-1].Date,
+			}
+		}
+	}
+
+	sort.Slice(data, func(i, j int) bool { return data[i].Contributions > data[j].Contributions })
+
+	fmt.Printf("先週(%s～%s)のGitHubのContribution数ランキングをお知らせします！\n\n", formatDate(&duration.From), formatDate(&duration.To))
+	for i, p := range data {
+		fmt.Printf("%d位: %s (%d contributions)\n", i+1, p.Name, p.Contributions)
+	}
+
+}
+
+func formatDate(t *time.Time) string {
+	return t.Format("2006/01/02")
 }
 
 func createClient() (graphql.Client, error) {
