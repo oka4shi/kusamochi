e68bb6e88e2556e0d372ec06f5cc3ad2e460a2cb
OKAMOTO Shigehiro <primary@oka4shi.dev>
2025-05-19 10:10:38 +0900

    feat: implement a library to post webhooks with files

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -2,6 +2,7 @@ package webhook
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
@@ -23,5 +24,98 @@ func Post(url string, body string) (*http.Response, error) {
 	}
 	defer response.Body.Close()
 
+	return response, err
+}
+
+type discordParams struct {
+	Content     string       `json:"content"`
+	Attachments []attachment `json:"attachments"`
+}
+
+type attachment struct {
+	ID          int    `json:"id"`
+	Description string `json:"description"`
+	Filename    string `json:"filename"`
+}
+
+type multipart struct {
+	ContentDisposition string
+	ContentType        string
+	Content            []byte
+}
+
+type File struct {
+	Name        string
+	Content     *bytes.Buffer
+	Description string
+}
+
+func PostWithFiles(url string, body string, files []File) (*http.Response, error) {
+	BOUNDARY := "KusamochiBoundary"
+
+	attachments := []attachment{}
+	for i, file := range files {
+		attachments = append(attachments, attachment{
+			ID:          i,
+			Description: file.Description,
+			Filename:    file.Name,
+		})
+	}
+
+	parts := []multipart{}
+	data := discordParams{
+		Content:     body,
+		Attachments: attachments,
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+
+	parts = append(parts, multipart{
+		ContentDisposition: fmt.Sprintf("form-data; name=\"payload_json\""),
+		ContentType:        "application/json",
+		Content:            jsonData,
+	})
+
+	for i, file := range files {
+		content := file.Content.Bytes()
+		parts = append(parts, multipart{
+			ContentDisposition: fmt.Sprintf("form-data; name=\"files[%d]\"; filename=\"%s\"", i, file.Name),
+			ContentType:        http.DetectContentType(content),
+			Content:            content,
+		})
+	}
+
+	reqBody := new(bytes.Buffer)
+	for _, part := range parts {
+		reqBody.WriteString(fmt.Sprintf("--%s\r\n", BOUNDARY))
+		reqBody.WriteString(fmt.Sprintf("Content-Disposition: %s\r\n", part.ContentDisposition))
+		if part.ContentType != "application/json" {
+			reqBody.WriteString(fmt.Sprintf("Content-Type: %s\r\n", part.ContentType))
+		}
+		reqBody.WriteString("\r\n")
+		reqBody.Write(part.Content)
+		reqBody.WriteString("\r\n")
+	}
+
+	reqBody.WriteString(fmt.Sprintf("--%s--\r\n", BOUNDARY))
+
+	request, err := http.NewRequest("POST", url, reqBody)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("Content-Type", fmt.Sprintf("multipart/form-data; boundary=%s", BOUNDARY))
+
+	//b, _ := io.ReadAll(request.Body)
+	//fmt.Println(string(b))
+	fmt.Println(request.Header)
+
+	webhook := &http.Client{}
+	response, err := webhook.Do(request)
+	if err != nil {
+		return nil, err
+	}
 	return response, err
 }
